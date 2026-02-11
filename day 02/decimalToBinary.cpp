67ba96130c08a3c37625057ec4b0de39d3826873
RahulMaurya90058 <rahulmaurya90058@gmail.com>
2026-02-11 16:02:41 +0530

    Create decimalToBinary.cpp
    not complete

---
+++ day 02/decimalToBinary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int Decimal_to_binary(int n){
+    
+}
+
+
+int main(){
+    int number;
+    cout<<"Enter a number to get their binary"<<endl;
+    cin>>number;
+    int result=Decimal_to_binary(number);
+    return 0;
+}
